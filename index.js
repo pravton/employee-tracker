2b47ade9fa5f0cd47358bdb25d91b9e9807b1036
PravTon <clintonsf@outlook.com>
2021-07-08 17:57:06 -0400

    added queries and prompts

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const db = require('./db/connection');
-const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
-const Roles = require('./lib/Roles');
-const cTable = require('console.table');
-
-const viewQuery = new Employee();
-const roles = new Roles();
+const { startPrompt } = require('./lib/prompts');
 
 db.connect(err => {
   if(err) throw err;
   console.log('Database Connected');
+  startPrompt();
 });
 
-roles.viewAllRoles();
