dc9cb17533ca71ca42c8981b51e71e4a01f967a4
PravTon <clintonsf@outlook.com>
2021-07-08 02:42:35 -0400

    added queries

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const  db = require('./db/connection');
+const db = require('./db/connection');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Roles = require('./lib/Roles');
+const cTable = require('console.table');
+
+const viewQuery = new Employee();
+const roles = new Roles();
 
 db.connect(err => {
   if(err) throw err;
   console.log('Database Connected');
 });
+
+roles.viewAllRoles();
